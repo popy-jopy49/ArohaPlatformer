825946438101dbb6a858180efe8dcab91670e208
TomS274 <128107938+TomS274@users.noreply.github.com>
2023-03-23 09:40:24 +1300

    Win

--- Aroha Platformer/Assets/Scripts/Win.cs
+++ Aroha Platformer/Assets/Scripts/Win.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Win : MonoBehaviour
 {
+    [SerializeField] private int levelToLoad;
+    private BoxCollider2D bCol;
+
+    private void Awake()
+    {
+        bCol = GetComponent<BoxCollider2D>();
+    }
 
     private void Update()
     {
-        
-    }
+        Collider2D[] hits = GameManager.I.PlayerCollisions;
 
-    public string levelToLoad = "";
-
-    private void OnTriggerEnter(Collider other)
-    {
-        SceneManager.LoadScene(levelToLoad);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit == bCol)
+            {
+                SceneManager.LoadScene(levelToLoad);
+            }
+        }
     }
 }
