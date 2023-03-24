23381ac8f372144d0f0087ae6f54952a922b5f0b
JoshAitkenhead <127905061+JoshAitkenhead@users.noreply.github.com>
2023-03-24 14:27:32 +1300

    dicks

--- Aroha Platformer/Assets/Scripts/GameManager.cs
+++ Aroha Platformer/Assets/Scripts/GameManager.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] private PlayerMovement player;
-    public PlayerMovement GetPlayerMovement() => player;
 
     [SerializeField] private Transform Respawn;
     [HideInInspector] public Collider2D[] PlayerCollisions;
