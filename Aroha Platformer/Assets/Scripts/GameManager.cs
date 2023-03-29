b028b2be8d28c572d2819fe1cbea311e390f94ec
popy-jopy49 <70320407+popy-jopy49@users.noreply.github.com>
2023-03-30 09:11:51 +1300

    Shard Indicators

--- Aroha Platformer/Assets/Scripts/GameManager.cs
+++ Aroha Platformer/Assets/Scripts/GameManager.cs
@@ -7,18 +7,11 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private Transform Respawn;
     [HideInInspector] public Collider2D[] PlayerCollisions;
     [SerializeField] private PlayerMovement Player;
-    private Transform canvas;
-
-    private void Awake()
-    {
-        canvas = GameObject.Find("Canvas").transform;
-    }
 
     public PlayerMovement GetPlayerMovement() => Player;
     public Vector2 GetRespawnPos() => Respawn.position;
     public void Indicate(float xPos)
     {
-        Destroy(Instantiate(GameAssets.I.IndicatorPrefab, Camera.main.WorldToScreenPoint(new Vector3(xPos, 10)), Quaternion.identity, canvas), 5);
     }
 
 }
